e5314ce1c99e068db465697a74faf276f52591cc
Tetsuro Kurita <tkurita@mac.com>
2016-07-06 09:31:48 +0900

    use enum instead of #define

--- ModuleLoaderConstants.h
+++ ModuleLoaderConstants.h
@@ -1,34 +1,43 @@
 #define STR(s) _STR(s)
 #define _STR(s) #s
 
-#define kModuleIsNotFound 1800
-#define kModuleLoaderInternalError 1802
-#define kFailedToParseVersionCondition 1803
-
 #define BUNDLE_ID CFSTR(STR(PRODUCT_BUNDLE_IDENTIFIER))
 #define MODULE_PATHS_KEY CFSTR("AdditionalModulePaths")
 
-#define kInDirectoryParam 'inDr'
-#define kOtherPathsParam 'ohPh'
-#define kForModuleParam 'forM'
-
 #define kModuleLoaderSuite  'Molo'
-#define kLoadModuleEvent	'loMo'
-#define kPrivateLoadModuleEvent 'PloM'
-#define kFindModuleEvent	'fdMo'
-#define kMakeLoaderEvent	'MKlo'
-#define kPrivateMakeLoaderEvent	'PMKl'
-#define kMakeLocaLoaderEvent 'MkLl'
-#define kSetAdditionalModulePathsEvent 'adMp'
-#define kModulePathsEvent 'gtPH'
-#define kVersionEvent 'Vers'
-#define kMakeModuleSpecEvent 'MkMs'
-#define kExtractDependenciesEvent 'Exdp'
-#define kReloadingParam 'pRLo'
-#define kVersionParam 'vers'
-#define kFromUseParam 'fmUs'
-#define kConditionParam 'ConD'
+
+enum ModuleLoaderCommands {
+    kLoadModuleEvent = 'loMo',
+    kFindModuleEvent = 'fdMo',
+    kPrivateLoadModuleEvent = 'PloM',
+    kMakeLoaderEvent = 'MKlo',
+    kPrivateMakeLoaderEvent	= 'PMKl',
+    kMakeLocaLoaderEvent = 'MkLl',
+    kExtractDependenciesEvent = 'Exdp',
+    kSetAdditionalModulePathsEvent = 'adMp',
+    kModulePathsEvent = 'gtPH',
+    kMakeModuleSpecEvent = 'MkMs',
+    kVersionEvent = 'Vers'
+};
+
+enum HandlerLabels {
+    kInDirectoryParam = 'inDr',
+    kOtherPathsParam = 'ohPh',
+    kConditionParam = 'ConD'
+};
+
+enum ModuleSpecifierProperties {
+    kReloadingParam = 'pRLo' ,
+    kVersionParam = 'vers' ,
+    kFromUseParam = 'fmUs'
+};
 
 enum {
 	typeModuleSpecifier = 'MoSp',
+};
+
+enum ModuleLoaderErros {
+    kModuleIsNotFound = 1800,
+    kModuleLoaderInternalError = 1802,
+    kFailedToParseVersionCondition = 1803
 };
